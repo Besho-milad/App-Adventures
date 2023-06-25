fde9258653e37d56eb2c7f0c9fd903f5a5893c3f
Besho-milad <137713215+Besho-milad@users.noreply.github.com>
2023-06-25 23:22:03 +0300

    Delete Project-final directory

--- Project-final/image_negative.m
+++
@@ -1,6 +0,0 @@
-function output_image = image_negative(input_image)
-
-output_image = 255 - input_image;
-output_image = uint8(output_image);
-
-end
--- Project-final/power_law.m
+++
@@ -1,6 +0,0 @@
-function output_image = power_law(input_image, gamma)
-
-output_image = double(input_image).^gamma;
-output_image = contrast_adjustment(output_image, 0, 255);   %post-processing: contrast adjustment (normalization) (-function in the folder-)
-
-end
